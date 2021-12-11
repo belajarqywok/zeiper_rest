61a9217fc289735fb7788b9f494a41e2e7484872
belajarqywok <belajarqywok@gmail.com>
2021-12-11 22:52:24 +0700

    push

---
+++ src/test/java/com/zeiper/zeiper_rest/ZeiperRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeiper.zeiper_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeiperRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
